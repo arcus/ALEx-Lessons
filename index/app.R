cec358206736a94c4bfc8a0c691094b1d4444af3
Joy Payton <pm0kjp@users.noreply.github.com>
2021-04-28 17:33:30 -0400

    update index

--- index/app.R
+++ index/app.R
@@ -5,29 +5,33 @@ library(shiny)
 ui <- fluidPage(
   includeCSS("www/bootstrap.min.css"),
   includeCSS("www/style.css"),
-  
+  tags$div(class = "chop_logo"),
     # Application title
-    titlePanel(tags$h2("Welcome to the ARCUS Modular Education Support System")),"Many people at CHOP have become interested in R and statistical analysis in R.  For Arcus Education, creating a learning environment where 100% of attendees spend 100% of their time working towards their",tags$em("own"),"goals at their own pace is of utmost importance.",
-    tags$br(),tags$br(),"Below you will find examples of how our system can provide this customized experience while being easily accessible anywhere in the world and maintaining reasonable throughput.",
-    tags$h3("R Beginner"),
-    "Here is an example lesson plan for a user who is just getting started with R. It covers the absolute basics including classes, logic, functions and graphics.",
+    titlePanel(HTML("<h2>Welcome to the <u>A</u>rcus <u>L</u>earning <u>Ex</u>change</h2>"), windowTitle = "Welcome to the Arcus Learning Exchange"),
+  tags$h3("Affectionately referred to as ALEx!"),
+  
+  "These tutorials are provided by the",
+  a("Arcus initiative", href = "https://arcus.chop.edu", target="_blank"),
+  "of Children's Hospital of Philadelphia (CHOP).",
+  tags$em("Note that the link to Arcus will only work for users on the CHOP network."),
+  tags$br(),tags$br(),
+  "As part of our work, the",
+  a("Arcus Education team", href = "https://education.arcus.chop.edu", target="_blank"),
+  "has developed ALEx as a way to provide just in time education for biomedical scientists at CHOP and beyond.",
+  tags$br(),tags$br(),
+  "Many people at CHOP have become interested in R and statistical analysis in R.  For Arcus Education, creating a learning environment where 100% of learners spend 100% of their time working towards their",tags$em("own"),"goals at their own pace is of utmost importance.",
+  tags$br(),tags$br(),
+  "You can browse our",  
+  a("lesson catalog",href="../../catalog/", target="_blank"),
+  "to discover our offerings.",
+  tags$br(),tags$br(),
+    "Want to get a quick preview of what ALEx provides? Here are some example lessons:",
     tags$br(),tags$br(),
-    a("Example R Beginner Lesson Plan",href="../Personalized-Learning-Plan/?&modules=sHXtfjmhu84EQfHM7VpLV&curricula=qiJ&modulesplit=MTMsMA~~"),
-    tags$h3("Introduction to Statistics in R"),
-    "Here is an example lesson plan for a user with some familiarity with R but who wants to learn to perform statistical analysis. It covers some fundamentals as well as practical application of statistical tests in R.",
-    tags$br(),tags$br(),
-    a("Example Statistics Lesson Plan",href="../Personalized-Learning-Plan/?&modules=yoyXvAlV&curricula=qyR&modulesplit=NSww"),
-    tags$h3("Example Lessons"),
-    "Here are some additional example lessons",
-    tags$br(),tags$br(),
-    a("Basic Building Blocks",href="../swirl-basic-building-blocks/"),tags$br(),tags$br(),
-    a("Variables",href="../variables/"),tags$br(),tags$br(),
-    a("Correlation and Linear Regression",href="../sb-correlation-and-linear-regression/"),
-    
-    tags$h3("Generate a Customized Curriculum"),
-    "Try your hand at generating a customized curriculum for yourself or others.",
-    tags$br(),tags$br(),
-    a("Custom Curriculum Generator",href="../Lesson-Generator/"),
+  tags$ul(
+    tags$li(a("Basic Building Blocks",href="../../swirl-basic-building-blocks/", target="_blank")),
+    tags$li(a("Variables",href="../../variables/", target="_blank")),
+    tags$li(a("Correlation and Linear Regression",href="../../correlation-and-linear-regression/", target="_blank"))
+  ),
 
 )
 
